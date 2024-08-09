add6de188d80210595d7b764d365ca14bdafa861
beatriz <128046453+abeazist@users.noreply.github.com>
2024-08-09 09:34:34 -0300

    salvando

---
+++ projetoReact/src/App.jsx
@@ -0,0 +1,14 @@
+
+function App() {
+
+  let nome = "Beatriz"
+
+  return (
+    <>
+          <h1>Seja bem vindo(a) {nome}</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Obcaecati deleniti minima pariatur sunt est fugit sint laudantium cumque adipisci consectetur facilis et quam doloremque iusto corporis natus, doloribus beatae consequuntur?</p>
+    </>
+  )
+}
+
+export default App
